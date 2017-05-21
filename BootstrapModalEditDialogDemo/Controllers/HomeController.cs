079d9d76c09f2008ed284d0e6014e4f837c2889e
mwhite102 <mwhite102@gmail.com>
2017-05-21 12:23:32 -0500

    Added JavaScript for editing items.  Also Added NuGet package Microsoft.jQuery.Unobtrusive.Ajax so the AjaxOptions will work.

--- BootstrapModalEditDialogDemo/Controllers/HomeController.cs
+++ BootstrapModalEditDialogDemo/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using BootstrapModalEditDialogDemo.Data;
+using BootstrapModalEditDialogDemo.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,86 @@ namespace BootstrapModalEditDialogDemo.Controllers
         {
             return View(ToDoDataService.GetToDoList());
         }
+
+        [HttpGet]
+        public ActionResult AddToDo()
+        {
+            ToDoModel model = new ToDoModel();
+            return PartialView("_ToDoEdit", model);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult AddToDo(ToDoModel model)
+        {
+            if (TryValidateModel(model))
+            {
+                // Do Save 
+                try
+                {
+                    if (ToDoDataService.InsertToDoItem(model))
+                    {
+                        // Reload the ToDoItem
+                        return PartialView("_ToDoDataRow", model);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+            }
+
+            // Failed Validation
+            Response.StatusCode = 400;
+            return PartialView("_ToDoEdit", model);
+        }
+
+
+        [HttpGet]
+        public ActionResult EditToDo(int id)
+        {
+            ToDoModel model = ToDoDataService.GetToDoById(id);
+            return PartialView("_ToDoEdit", model);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult EditToDo(ToDoModel model)
+        {
+            if (TryValidateModel(model))
+            {
+                // Do Save 
+                try
+                {
+                    if (ToDoDataService.UpdateToDoItem(model))
+                    {
+                        // Reload the ToDoItem
+                        return PartialView("_ToDoDataRow", model);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+            }
+
+            // Failed Validation
+            Response.StatusCode = 400;
+            return PartialView("_ToDoEdit", model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            if (ToDoDataService.DeleteToDoItem(id))
+            {
+                return new EmptyResult();
+            }
+            else
+            {
+                Response.StatusCode = 400;
+                return new EmptyResult();
+            }
+        }
+
 
         public ActionResult About()
         {
---
+++ BootstrapModalEditDialogDemo/Scripts/app.js
@@ -0,0 +1,52 @@
+﻿//-----------------------------------------------------------------
+// ToDo Editing Code
+//-----------------------------------------------------------------
+
+// Wire put the click event for 'Edit' links for each ToDo row
+$(document).on('click', '.todo_editor-container', function (event) {
+    event.preventDefault();
+
+    var url = "/Home/EditTodo";
+    var id = $(this).attr('data-id');
+
+    $.get(url + '/' + id, function (data) {
+        // Load the data (the partial view) into the modal dialog
+        $('#editor-content-container').html(data);
+        // Show the modal dialog
+        $('#editor-container').modal('show');
+    });   
+});
+
+// Called when the ToDo edit is successful
+function toDoEditSuccess(data, status, xhr) {
+    // Hide the modal dialog
+    $('#editor-container').modal('hide');
+    // Clear the contents of the modal dialog
+    $('#editor-content-container').html("");
+}
+
+// Called when the ToDo edit fails
+function toDoEditFailure(xhr, status, error) {
+    // Reload the modal dialog content with the response
+    $('#editor-content-container').html(xhr.responseText);
+    // Show the modal dialog
+    $('#editor-container').modal('show');
+}
+
+//-----------------------------------------------------------------
+// ToDo Creation Code
+//-----------------------------------------------------------------
+
+// Wire put the click event for 'Add' button
+//$(document).on('click', '#add-button', function (event) {
+//    event.preventDefault();
+
+//    var url = "/Home/AddTodo";
+
+//    $.get(url , function (data) {
+//        // Load the data (the partial view) into the modal dialog
+//        $('#editor-content-container').html(data);
+//        // Show the modal dialog
+//        $('#editor-container').modal('show');
+//    });
+//});
